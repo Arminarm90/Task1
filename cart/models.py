0f93960ddac236eb0b8cdd06a0f95311a37c5cf1
Armin Mozafary <armin.arm90@gmail.com>
2024-05-04 23:53:30 +0330

    Cart model updated

--- cart/models.py
+++ cart/models.py
@@ -9,6 +9,7 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
     
 
     def __str__(self):
